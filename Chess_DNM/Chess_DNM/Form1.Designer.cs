549f4ec4740f6a71d249533201f484e4464e9350
DarrenMoody <moodytoons@gmail.com>
2015-03-26 01:42:25 -0600

    One last small change before bedtime

--- Chess_DNM/Chess_DNM/Form1.Designer.cs
+++ Chess_DNM/Chess_DNM/Form1.Designer.cs
@@ -1069,9 +1069,10 @@
 			// 
 			this.label1.AutoEllipsis = true;
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(75, 11);
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label1.Location = new System.Drawing.Point(72, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(47, 13);
+			this.label1.Size = new System.Drawing.Size(49, 15);
 			this.label1.TabIndex = 2;
 			this.label1.Text = "Difficulty";
 			// 
@@ -1081,10 +1082,10 @@
 			this.textBox1.Font = new System.Drawing.Font("Neuropol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.textBox1.ForeColor = System.Drawing.Color.Lime;
 			this.textBox1.Location = new System.Drawing.Point(72, 27);
-			this.textBox1.MaximumSize = new System.Drawing.Size(50, 30);
-			this.textBox1.MinimumSize = new System.Drawing.Size(50, 30);
+			this.textBox1.MaximumSize = new System.Drawing.Size(50, 25);
+			this.textBox1.MinimumSize = new System.Drawing.Size(50, 25);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(50, 30);
+			this.textBox1.Size = new System.Drawing.Size(50, 25);
 			this.textBox1.TabIndex = 3;
 			this.textBox1.Text = "100";
 			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
